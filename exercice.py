dd925669741a67c4f1decfd1c839e39f03ee1205
homer-crypto <homer.nde-tsakou@polymtl.ca>
2020-10-26 19:57:08 -0400

    ex1-2

--- exercice.py
+++ exercice.py
@@ -8,8 +8,40 @@ PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80
 
 # TODO: Définissez vos fonction ici
 
+# Version du prof:
+def exercice1(file1, file2):
+    with open(file1, encoding="utf-8") as f1, open(file2, encoding="utf-8") as f2:
+        for index, line1 in enumerate(f1):
+            line2 = f2.readline()
+            if line1 != line2:
+                print(f"les fichiers sont differents sur la ligne {index + 1}")
+                break
+
+def exercice2(file1, new_file):
+    with open(file1, encoding="utf-8") as f1, open(new_file, 'w', encoding="utf-8") as nf:
+        for line in f1:
+            for i in line:
+                nf.write(i)
+                if i == " ":
+                    nf.write("   ")
+
+"""
+version du prof:
+
+def exercice2(file1, new_file):
+    with open(file1, encoding="utf-8") as f1, open(new_file, 'w', encoding="utf-8") as nf:
+        for line in f1:
+            words = line.split()
+            triple_line = "   ".join(words)
+            nf.write(triple_line)
+
+"""
+
+
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    exercice1(r"C:\Users\Homer\Documents\GitHub\c04-ch8-exercices-homer-crypto\exemple.txt",
+              r"C:\Users\Homer\Documents\GitHub\c04-ch8-exercices-homer-crypto\exemple2")
 
-    pass
+    exercice2(r"C:\Users\Homer\Documents\GitHub\c04-ch8-exercices-homer-crypto\exemple.txt", "nouveau_fichier.txt")
